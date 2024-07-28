140fa43d30fe028331f542cba26f5becf920221b
JennyPao18 <jenniferpaolapirirbor12@gmail.com>
2024-07-28 16:09:02 -0600

    first commit

---
+++ src/main/java/com/example/RenderGitHub/RenderGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.RenderGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderGitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RenderGitHub/RenderGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RenderGitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
